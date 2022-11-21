a2dc9127b32278358c352f7bc8192234f672e266
Raykov16 <raykov_git@abv.bg>
2022-11-21 13:18:51 +0200

    Initial commit

---
+++ EmployeesApp/Data/Entities/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeesApp.Data.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public int? ManagerId { get; set; }
+        public virtual Employee? Manager { get; set; }
+        public int Age { get; set; }
+        public int RoleId { get; set;}
+        public virtual Role Role { get; set; }
+        public string Email { get; set; }
+        public string SurName { get; set; }
+        public string Name { get; set; }
+
+        public virtual HashSet<Team> Teams { get; set; }
+    }
+}
---
+++ EmployeesApp/Data/Entities/Team.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesApp.Data.Entities
+{
+    public class Team
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual HashSet<Employee> Employees { get; set; }
+    }
+}
